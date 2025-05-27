7b464616b5d040e5b69d72a5d358986d8e4c1c82
Skrmr <l.vicinanza13@studenti.unisa.it>
2025-05-27 12:20:25 +0200

    commit di prova

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+//display menu
+void showmenu(){
+    printf("\n---  Planning and Monitoring System\n");
+    printf("1. Add study activity\n");
+    printf("2. Upgrade progress\n");
+    printf("3. View activities\n");
+    printf("4. Generate weekly report\n");
+    printf("0. Close\n");
+    printf("Scelta: ");
+}
+
+int main(){
+    //switch
+    
+}
